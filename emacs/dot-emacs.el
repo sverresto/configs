82c2329efa13551de4a5d7ca883f192242eb26bd
Sverre Stoltenberg <sverrest@met.no>
2021-09-15 17:04:34 +0200

    latest iteration

--- emacs/dot-emacs.el
+++ emacs/dot-emacs.el
@@ -1,15 +1,18 @@
+;;; sverrest's .emacs file
+;;
 ;; This installs 'use-package' automatically
 (require 'package)
 (add-to-list 'package-archives
-             '("melpa-stable" . "http://stable.melpa.org/packages/") t)
+             '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 (add-to-list 'package-archives
-             '("melpa" . "http://melpa.org/packages/") t)
+             '("melpa" . "https://melpa.org/packages/") t)
 (package-initialize)
 
 (unless (package-installed-p 'use-package)
   (package-refresh-contents)
   (package-install 'use-package))
 
+;; Some standard configuration
 (if (display-graphic-p)
     (progn
       ;; if graphic
@@ -33,6 +36,14 @@
       display-time-24hr-format t
       auth-sources '("~/.authinfo"))
 
+(setq lsp-ui-doc-show-with-cursor nil)
+;; (setq gc-cons-threshold (* 100 1024 1024)
+;;       read-process-output-max (* 1024 1024)
+;;       treemacs-space-between-root-nodes nil
+;;       company-idle-delay 0.0
+;;       company-minimum-prefix-length 1
+;;       lsp-idle-delay 0.1)  ;; clangd is fast
+
 (set-face-attribute 'default nil
 		    :font "Inconsolata-17")
 
@@ -44,7 +55,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(flycheck magithub lsp-pyright color-theme-modern k8s-mode company-ansible dired-efap yaml-mode ansible-vault ansible-doc ansible yasnippet-snippets lsp-latex dark-souls go-snippets yasnippet go-mode markdown-mode+ mardown-mode+ markdown-toc x company lsp-ui selectrum which-key use-package fill-column-indicator)))
+   '(lsp helm-xref projectile helm-lsp flycheck magithub lsp-pyright color-theme-modern k8s-mode company-ansible dired-efap yaml-mode ansible-vault ansible-doc ansible yasnippet-snippets lsp-latex dark-souls go-snippets yasnippet go-mode markdown-mode+ mardown-mode+ markdown-toc x company lsp-ui selectrum which-key use-package fill-column-indicator)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -66,7 +77,16 @@
 (use-package yasnippet-snippets :ensure)
 (use-package go-snippets :ensure)
 
-;; give a blue horisontal line when it is time to wrap
+;;bells and wistle for the LSP modes
+(use-package lsp-treemacs :ensure)
+(use-package helm-lsp  :ensure)
+(use-package projectile :ensure)
+(use-package hydra :ensure)
+(use-package avy :ensure)
+(use-package which-key :ensure)
+(use-package helm-xref :ensure)
+
+;; give a blue horisontal line where it is time to wrap
 (use-package fill-column-indicator
   :ensure
   :config
@@ -82,18 +102,21 @@
   :ensure
   :config
   (setq company-dabbrev-downcase nil) ; expand camelCase
-  :hook
-  (yaml-mode-hook . ansible))
-
+  (add-hook 'yaml-mode-hook #'ansible))
+ 
 (use-package k8s-mode
  :ensure
  :config
  (setq k8s-search-documentation-browser-function 'browse-url-firefox)
- :hook
- (k8s-mode . yas-minor-mode))
+ (add-hook 'k8s-mode #'yas-minor-mode))
 
 ;; Work in progress
 (use-package dap-mode :ensure) ;; 
+
+(use-package lsp-mode
+  :ensure
+  :config
+  (add-hook 'lsp-mode-hook #'lsp-enable-which-key-integration))
 
 (use-package lsp-ui :ensure)
 
@@ -101,21 +124,18 @@
 (use-package lsp-pyright
   :ensure
   :config
-  ;; (add-hook 'python-mode-hook #'yas-minor-mode)
-  ;;(add-hook 'python-mode-hook #'lsp-deferred))
-  :hook
-  (python-mode-hook . yas-minor-mode)
-  (python-mode-hook . lsp-deferred))
+  (add-hook 'python-mode-hook #'yas-minor-mode)
+  (add-hook 'python-mode-hook #'lsp-deferred))
 
 ;; LSP is using this
 (use-package company
   :ensure
   :bind
   (:map company-active-map
-              ("C-n". company-select-next)
-              ("C-p". company-select-previous)
-              ("M-<". company-select-first)
-              ("M->". company-select-last))
+        ("C-n". company-select-next)
+        ("C-p". company-select-previous)
+        ("M-<". company-select-first)
+        ("M->". company-select-last))
   ;(:map company-mode-map
   ;      ("<tab>". tab-indent-or-complete)
   ;      ("TAB". tab-indent-or-complete))
@@ -154,3 +174,4 @@
 (set-face-attribute 'lsp-signature-face nil :font "Inconsolata-17")
 (set-face-attribute 'fixed-pitch-serif nil :font "Inconsolata-17")
 (set-face-attribute 'fixed-pitch nil :font "Inconsolata-17")
+
